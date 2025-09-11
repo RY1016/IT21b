f0b3ef683c5056e9c538180f7b0b31820a20f44d
RY1016 <20241016@nbsc.edu.ph>
2025-09-11 19:34:37 +0800

    added menu items for factorial.js

--- factorial.js
+++ factorial.js
@@ -1,1 +1,8 @@
 console.log("Factorial Application");
+
+console.log("---------------------------");
+console.log("Factorial Application");
+console.log("1. Say Hello");
+console.log("2. Factorial");
+console.log("3. Exit Application");
+console.log("---------------------------");
