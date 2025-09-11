986094fdec1e965631b156d833c5ae1166fd21e1
RY1016 <20241016@nbsc.edu.ph>
2025-09-11 19:49:49 +0800

    Added conditional statement under rl code block

--- factorial.js
+++ factorial.js
@@ -12,7 +12,15 @@ console.log("2. Factorial");
 console.log("3. Exit Application");
 console.log("---------------------------");
 
-rl.question("Enter your choice (1-3): ", choice => {
-    console.log("You selected: " + choice);
-    rl.close();
+rl.question("Enter your choice (1-3): ", choice =>{
+    if (choice === "1"){
+        console.log("Hello");
+    } else if (choice === "2"){
+        console.log("Factorial");
+    } else if (choice === "3"){
+        console.log("Exiting Application");
+    } else{
+        console.log("Invalid Choice. Please Try Again");
+    }
+
 });
